e6126dfa8c6ea095520ad50917b9ccce101265a5
Keenan Flynn <k.p.flynn70@gmail.com>
2024-02-17 14:35:48 -0600

    deploy(env): env vars

---
+++ tunelink/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  songkickKey: '8NUFX7nR2KeXLUKt',
+  ticketmasterKey: 'hXhy6hDyIa696MFmRDkkvSRJqhknBt4U',
+  ticketmasterSecret: 'u1pPGdD1LFK764GV',
+  mapboxKey: 'pk.eyJ1Ijoid2luaXZpcyIsImEiOiJja3p6MWdhc20wNXNhM2pzMDd2b3B5bHczIn0.HZ585lasEzRwmZVPWK2aAg',
+  spotifyClientId: 'afbdab91d8214fbb979660933ccd05e1',
+  spotifyClientSecret: 'f3007731ce2742939c331ee1564ceb06',
+};
---
+++ tunelink/src/environments/environment.ts
@@ -0,0 +1,22 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  songkickKey: '8NUFX7nR2KeXLUKt',
+  ticketmasterKey: 'hXhy6hDyIa696MFmRDkkvSRJqhknBt4U',
+  ticketmasterSecret: 'u1pPGdD1LFK764GV',
+  mapboxKey: 'pk.eyJ1Ijoid2luaXZpcyIsImEiOiJja3p6MWdhc20wNXNhM2pzMDd2b3B5bHczIn0.HZ585lasEzRwmZVPWK2aAg',
+  spotifyClientId: 'afbdab91d8214fbb979660933ccd05e1',
+  spotifyClientSecret: 'f3007731ce2742939c331ee1564ceb06'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
